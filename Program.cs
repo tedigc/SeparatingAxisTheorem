51cf8d84db65ed2c6dc5451ad2ac53f9d775b128
ted <ted.cater@acresoftware.com>
2020-11-11 18:35:22 +0000

    Initialise project

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace PolygonCollision {
+    public static class Program {
+        [STAThread]
+        static void Main() {
+            using var game = new Game();
+            game.Run();
+        }
+    }
+}
---
+++ Source/Game.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace PolygonCollision {
+    public class Game : Microsoft.Xna.Framework.Game {
+        
+        public const int Width = 400;
+        public const int Height = 300;
+        public const string Title = "Polygon Collision";
+        
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        public Game() {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize() {
+            _graphics.PreferredBackBufferWidth  = Width;
+            _graphics.PreferredBackBufferHeight = Height;
+            _graphics.ApplyChanges();
+            Window.Title = Title;
+            base.Initialize();
+        }
+
+        protected override void LoadContent() {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime) {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape)) Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime) {
+            GraphicsDevice.Clear(Color.Black);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
